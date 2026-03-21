d1675ec371dca29f8d181d361ad0d63ae2578ac8
tsungie <misstsungie@gmail.com>
2026-03-21 09:10:35 +0200

    Fixed dotenv path for PythonAnywhere

--- roots_core/settings.py
+++ roots_core/settings.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+from dotenv import load_dotenv
+
+env_path = os.path.join(BASE_DIR, ".env")
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
